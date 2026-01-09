1109b88eab4e716c50541b0871a8ec8d55695de2
Frizlab <fload@me.com>
2026-01-09 22:59:37 +0100

    Fix compilation on ancient macOS

--- Sources/XDG/BaseDirectories+FindFiles.swift
+++ Sources/XDG/BaseDirectories+FindFiles.swift
@@ -27,7 +27,9 @@ public extension BaseDirectories {
 		return try Array(findAll(path, in: [runtimeDirPrefixed.get()]))
 	}
 	
-	internal func findAll(_ searched: FilePath, in candidates: [FilePath]) throws -> some Collection<FilePath> {
+	/* `some` is only available starting from macOS 10.15 */
+//	internal func findAll(_ searched: FilePath, in candidates: [FilePath]) throws -> some Collection<FilePath> {
+	internal func findAll(_ searched: FilePath, in candidates: [FilePath]) throws -> LazyFilterSequence<[FilePath]> {
 		let fileManager = fileManagerFactory()
 		return try candidates.map{ try $0.lexicallyResolving(searched) }.lazy.filter{ $0.existsNotDir(with: fileManager) }
 	}
